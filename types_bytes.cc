638a71d2f387f352887cd429cbda5e8827c7c7cb
miaohongyuan <miaohongyuan@autohome.com.cn>
2016-09-14 15:00:43 +0800

    types_bytes.cc: add sizeof(type)

---
+++ types_bytes.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "bool: " << sizeof(bool) << endl;
+
+    cout << "char: " << sizeof(char) << endl;
+    cout << "unsigned char: " << sizeof(unsigned char) << endl;
+
+    cout << "int: " << sizeof(int) << endl;
+    cout << "unsigned int: " << sizeof(unsigned int) << endl;
+
+    cout << "long: " << sizeof(long) << endl;
+    cout << "unsigned long: " << sizeof(unsigned long) << endl;
+    cout << "long long: " << sizeof(long long) << endl;
+    cout << "unsigned long long: " << sizeof(unsigned long long) << endl;
+    
+    cout << "float: " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+
+    return 0;
+}
