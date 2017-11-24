21efe23fa7c05fce90dac214e857ac99f2ed1164
mhyeagle <tianyashuibin@163.com>
2017-11-24 21:23:48 +0800

    update jsoncpp

--- jsoncpp/writer.cpp
+++ jsoncpp/writer.cpp
@@ -11,12 +11,32 @@ int main() {
 
     person["name"] = "hello";
     person["age"] = "13";
-
+    root.append(person);
+    person["name"] = "world";
+    person["age"] = "15";
+    person["sex"] = "female";
     root.append(person);
 
     string json_str = writer.write(root);
     cout << "value:" << endl;
     cout << json_str << endl;
+
+    cout << "***************" << endl;
+
+    cout << "root size: " << root.size() << endl;
+    for (int i = 0; i < root.size(); ++i) {
+        Json::Value::Members mem = root[i].getMemberNames();
+        for (auto iter = mem.begin(); iter != mem.end(); iter++) {
+            cout << "key and value: " << endl;
+            cout << "*iter: " << *iter << " type: " << (*iter) << endl;
+            cout << "root[i][*iter]: " << root[i][*iter] << " type: " << root[i][*iter].type() << endl;
+        }
+    }
+    
+
+
+
+    cout << "***************" << endl;
 
     root.clear();
     cout << "root is empty: " << root.empty() << endl;
