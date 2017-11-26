d73872c3197366ad47792ec600d98533b2e5ec15
mhyeagle <tianyashuibin@163.com>
2017-11-26 18:01:08 +0800

    libevent update

--- libevent/test.cc
+++ libevent/test.cc
@@ -7,6 +7,16 @@ using namespace std;
 
 int main() {
     cout << event_get_version() << endl;
+    cout << "support method:" << endl;
+    const char **methods = event_get_supported_methods();
+    for (int i = 0; methods[i] != NULL; i++) {
+        cout << methods[i] << endl;
+    }
+
+    cout << "******" << endl;
+    event_base *base = event_base_new();
+    cout << event_base_get_method(base) << endl;
+    event_base_free(base);
 
     return 0;
 }
