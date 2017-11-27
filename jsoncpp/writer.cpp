66a920da0ca0d58f1573f574f159f7e4a7e17911
mhyeagle <tianyashuibin@163.com>
2017-11-27 20:31:35 +0800

    update

--- jsoncpp/writer.cpp
+++ jsoncpp/writer.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int main() {
     Json::Value root;
+    cout << "root original: " << root << endl;
     Json::Value person;
     Json::FastWriter writer;
 
     person["name"] = "hello";
     person["age"] = "13";
+    person["age"] = Json::nullValue;
     root.append(person);
     person["name"] = "world";
     person["age"] = "15";
@@ -38,11 +40,28 @@ int main() {
 
     cout << "***************" << endl;
 
+    //root.clear();
     root.clear();
+    root.clear();
+
+    cout << "&&&&&&" << endl;
+    cout << "root is: " << root << endl;
+    if (root.type() == Json::arrayValue) {
+        cout << "root.type() = Json::arrayValue" << endl;
+    }
+    cout << "&&&&&&" << endl;
+
+    root = Json::nullValue;
     cout << "root is empty: " << root.empty() << endl;
     json_str = writer.write(root);
     cout << "value_null:" << endl;
     cout << json_str << endl;
+    if (root == Json::nullValue) {
+        cout << "root = Json::nullValue" << endl;
+    } else {
+        cout << "root != Json::nullValue" << endl;
+        cout << root << endl;
+    }
 
 
     return 0;
