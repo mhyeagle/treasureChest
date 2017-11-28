897b0d1a4ca4da18a683aa6350c1dceccab3e793
mhyeagle <tianyashuibin@163.com>
2017-11-28 10:10:42 +0800

    bufferevent_test

---
+++ libevent/bufferevent_test.cc
@@ -0,0 +1,39 @@
+#include <event2/event.h>
+#include <event2/bufferevent.h>
+#include <sys/socket.h>
+#include <string>
+
+void eventcb(struct bufferevent *bev, short events, void *ptr) {
+    if (events & BEV_EVENT_CONNECTED) {
+        //start reading
+        cout << "we should start reading or writing here." << endl;
+    } else if (events & BEV_EVENT_ERROR) {
+        cout << "an error occured while connect." << endl;
+    }
+}
+
+int main() {
+    struct event_base *base;
+    struct bufferevent *bev;
+    struct sockaddr_in sin;
+
+    base = event_base_new();
+
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_addr.s_addr = htonl(0x7f000001);
+    sin.sin_port = htons(8080);
+
+    bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+
+    bufferevent_setcb(bev, NULL, NULL, eventcb, NULL);
+
+    if (bufferevent_socket_connect(bev, (struct sockaddr_in*)&sin, sizeof(sin)) < 0) {
+        //Error starting connection
+        bufferevent_free(bev);
+        return -1;
+    }
+
+    event_base_dispatch(base);
+
+    return 0;
+}
